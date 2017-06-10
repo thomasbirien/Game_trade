de89f61651c62377df696497f502ece83f0ced05
thomasbirien <thomasbirien@gmail.com>
2017-06-10 19:40:20 +0200

    generate controller offer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'offers/show'
+
+  get 'offers/new'
+
+  get 'offers/create'
+
+  get 'offers/edit'
+
+  get 'offers/update'
+
+  get 'offers/destroy'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
