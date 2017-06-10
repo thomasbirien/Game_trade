2fb14f7ae651bc45ff5d865f5022a1127788221c
thomasbirien <thomasbirien@gmail.com>
2017-06-10 19:37:20 +0200

    deleted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get 'games/index'
-
-  get 'games/show'
-
-  get 'games/new'
-
-  get 'games/create'
-
-  get 'games/edit'
-
-  get 'games/update'
-
-  get 'games/destroy'
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
