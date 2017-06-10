590ff8acfa8e2d6e908b20f00b836270851a9291
thomasbirien <thomasbirien@gmail.com>
2017-06-10 19:40:58 +0200

    deleted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
-  get 'offers/show'
-
-  get 'offers/new'
-
-  get 'offers/create'
-
-  get 'offers/edit'
-
-  get 'offers/update'
-
-  get 'offers/destroy'
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
