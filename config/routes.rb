d31ff065099a914386d9826d70e31cf3aae502db
thomasbirien <thomasbirien@gmail.com>
2017-06-11 18:08:49 +0200

    added routes root to games index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  root to: 'games#index'
   resources :games, only: [:index, :show] do
     resources :offers, only: [:new, :create, :show]
   end
