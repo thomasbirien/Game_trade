bfa9a6e19572ff0349e64912ec3641d762cc2d52
thomasbirien <thomasbirien@gmail.com>
2017-11-06 22:16:00 +0100

    added method in games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,17 +8,38 @@ class GamesController < ApplicationController
   end
 
   def new
+    @game = Game.new
   end
 
   def create
+    @game = current_user.games.new(game_params)
+
+    if @game.save
+      redirect_to profile_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @game = Game.find(params[:id])
   end
 
   def update
+    @game = Game.find(params[:id])
+    if @game.update(game_params)
+      redirect_to profiles_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:game_name, :plateforme, :picture, :condition, :comment, :owner_id)
   end
 end
