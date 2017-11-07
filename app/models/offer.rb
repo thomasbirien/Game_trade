8dad19d213f3eb5b654c38b92565c8cf80e426ac
thomasbirien <thomasbirien@gmail.com>
2017-11-07 18:08:10 +0100

    added game_exchange_id for offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,5 @@
 class Offer < ApplicationRecord
   belongs_to :game
+  belongs_to :game_exchange, class_name: "Game", foreign_key: "game_exchange_id"
   belongs_to :user
 end
--- db/migrate/20170610173159_add_column_to_offers.rb
+++ db/migrate/20170610173159_add_column_to_offers.rb
@@ -1,5 +1,6 @@
 class AddColumnToOffers < ActiveRecord::Migration[5.0]
   def change
     add_column :offers, :user_id, :integer
+    add_column :offers, :game_exchange_id, :integer
   end
 end
